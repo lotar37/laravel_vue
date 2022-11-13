726c6c212895c6d772ba4c3545bc0b5a0792b07a
SergeyLyakhovetsky <lotar37@gmail.com>
2022-11-13 14:11:35 +0300

    vue router использование

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import router from "./router";
+import Index from "./components/Index";
 
 require('./bootstrap');
 
@@ -6,7 +8,8 @@ require('./bootstrap');
 const app = new Vue({
     el: '#app',
     components : {
-
-    }
+        Index,
+    },
+    router
 
 });
---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+    Post component
+</div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
